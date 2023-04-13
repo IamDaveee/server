9b4859de79f66d9fdef04a668633c5c2c3c8e94b
IamDaveee <121976848+IamDaveee@users.noreply.github.com>
2023-04-13 23:07:17 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const app = express()
+app.use(express.json())
+app.use(express.static('Public-Webpage'))
+require('dotenv').config();
+
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
+stripe.api_key = 'sk_test_51MYCEYLmmPzbtI7RRp4lupxYnPKSDd0UcWe4kFZ2TUsV6uDS8MVDQ6yZGe7TqiIVkqqdKwOPx04VcmVRwmph09zg00u2ZqdmWU'
+
+
+const storeItems = new Map([
+  [1, { priceInCents: 999, name: "V1 Preset" }],
+  [2, { priceInCents: 999, name: "V2 Preset" }],
+  [3, { priceInCents: 1699, name: "Ultimate Bundle" }],
+])
+
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: req.body.items.map(item => {
+        const storeItem = storeItems.get(item.id)
+        return {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: storeItem.name,
+            },
+            unit_amount: storeItem.priceInCents,
+          },
+          quantity: item.quantity,
+        }
+      }),
+      success_url: `${process.env.CLIENT_URL}/success.html`,
+      cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
+    })
+    res.json({ url: session.url })
+  } catch (e) {
+    res.status(500).json({ error: e.message })
+  }
+})
+
+app.listen(3000)
